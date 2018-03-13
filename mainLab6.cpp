0863c24909c2078d29bedfff5e5980e1ca11d16e
bManrique17 <bmanrique17@localhost.localdomain>
2018-03-12 21:06:31 -0600

    Movimiento validado

--- mainLab6.cpp
+++ mainLab6.cpp
@@ -24,28 +24,95 @@ int main(){
     initscr();
 
     start_color();
-
     
-
     Escenario* mapa = new Escenario("ASDGFJDH");
-    Jugador* jugador = new Jugador("JUlicho",0,1,0,0);
-    Jugador* boot1 = new Jugador("boot1",0,0,0,12);
-    Jugador* boot2 = new Jugador("boot2",0,0,6,6);
-    Jugador* boot3 = new Jugador("boot3",0,0,10,0);
-    Jugador* boot4 = new Jugador("boot4",0,0,10,12);
+    Jugador* jugador = new Jugador("  J  ",0,1,0,0);
+    Jugador* boot1 = new Jugador(" B 1 ",0,0,0,12);
+    Jugador* boot2 = new Jugador(" B 2 ",0,0,6,6);
+    Jugador* boot3 = new Jugador(" B 3 ",0,0,10,0);
+    Jugador* boot4 = new Jugador(" B 4 ",0,0,10,12);
     mapa->setItem(jugador,jugador->getX(),jugador->getY());    
     mapa->setItem(boot1,boot1->getX(),boot1->getY());
     mapa->setItem(boot2,boot2->getX(),boot2->getY());
     mapa->setItem(boot3,boot3->getX(),boot3->getY());
     mapa->setItem(boot4,boot4->getX(),boot4->getY());
-
+       
     imprimirMapa(mapa);
-    /*
-    move(10,20);
-    printw("asd");
-    */
-    getch();
     refresh();
+    while(true){
+        int entrada = getch();
+        int futX,futY;
+        switch(entrada){
+            case 119:{//w
+                futX = jugador->getX()-1;   
+                futY = jugador->getY();
+
+                if(futX<0 || futX>10 || futY<0 || futY>12)
+                    break;
+
+                if( !(futX%2 != 0 && futY%2 != 0) && mapa->getMatriz()[futX][futY] == NULL){                    
+                    jugador->setX(futX);
+                    jugador->setY(futY);
+                    mapa->setItem(NULL,jugador->getX()+1,jugador->getY());
+                    mapa->setItem(jugador,jugador->getX(),jugador->getY());
+                    imprimirMapa(mapa);      
+                }          
+            }
+            break;
+            case 100:{//d                
+                futX = jugador->getX();   
+                futY = jugador->getY()+1;
+
+                if(futX<0 || futX>10 || futY<0 || futY>12)
+                    break;
+
+                if( !(futX%2 != 0 && futY%2 != 0) && mapa->getMatriz()[futX][futY] == NULL){
+                    jugador->setX(futX);
+                    jugador->setY(futY);
+                    mapa->setItem(NULL,jugador->getX(),jugador->getY()-1);
+                    mapa->setItem(jugador,jugador->getX(),jugador->getY());
+                    imprimirMapa(mapa);
+                }
+            }
+            break;
+            case 97:{//a               
+                futX = jugador->getX();   
+                futY = jugador->getY()-1;
+
+                if(futX<0 || futX>10 || futY<0 || futY>12)
+                    break;
+
+                if( !(futX%2 != 0 && futY%2 != 0) && mapa->getMatriz()[futX][futY] == NULL){
+                    jugador->setX(futX);
+                    jugador->setY(futY);
+                    mapa->setItem(NULL,jugador->getX(),jugador->getY()+1);
+                    mapa->setItem(jugador,jugador->getX(),jugador->getY());
+                    imprimirMapa(mapa);
+                }
+            }
+            break;
+            case 115:{//s   
+                futX = jugador->getX()+1;   
+                futY = jugador->getY();        
+
+                if(futX<0 || futX>10 || futY<0 || futY>12)
+                    break;
+
+                if( !(futX%2 != 0 && futY%2 != 0) && mapa->getMatriz()[futX][futY] == NULL){                    
+                    jugador->setX(futX);
+                    jugador->setY(futY);
+                    mapa->setItem(NULL,jugador->getX()-1,jugador->getY());
+                    mapa->setItem(jugador,jugador->getX(),jugador->getY());
+                    imprimirMapa(mapa);            
+                }
+            }
+            break;
+
+        }
+        entrada = 0;
+    }
+    getch();
+    
     endwin();
     exit(1);
     return 0;
@@ -63,14 +130,33 @@ void imprimirMapa(Escenario* mapa){
             if(i%2 != 0 && j%2 != 0){
                 attrset(COLOR_PAIR(2));
             }
+            
+                printw("     ");    
+            
+            
+            move(i*2,j*5+5);
+        }
+    }
+
+
+    for(int i=0 ; i<11 ; i++){
+        for(int j=0 ; j<13 ; j++){
+            attrset(COLOR_PAIR(1));
+            move(i*2 + 2,j*5 + 5);
+            if(i%2 != 0 && j%2 != 0){
+                attrset(COLOR_PAIR(2));
+            }
             Item* objeto = mapa->getItem(i,j);
             if(objeto != NULL){
                 Jugador* objeto2 = dynamic_cast<Jugador*>(objeto);
-                if(objeto2 != NULL)
-                    if(objeto2->getTipoControlador() == 0)
-                        printw("  B  ");
-                    else
-                        printw("  J  ");
+                if(objeto2 != NULL){
+                
+                    string salida = objeto2->getNombre();
+                    for(int k=0 ; k<5 ; k++){
+                        char l = salida[k];
+                        printw("%c",l);
+                    }                    
+                }
             }else{
                 printw("     ");    
             }
@@ -79,7 +165,7 @@ void imprimirMapa(Escenario* mapa){
         }
     }
     
-
+    refresh();
 
 }
 
